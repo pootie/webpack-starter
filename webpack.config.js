e11d4d0f9b8bd03897576aa0ed4aee1666fd35db
Chris Tran <chris@grgichtran.com>
2017-09-06 05:09:25 +1000

    automatic-browser-refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const commonConfig = {
   // Entries have to resolve to files! They rely on Node
   // convention by default so if a directory contains *index.js*,
   // it resolves to that.
@@ -20,6 +22,47 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack starter'
-    })
+    }),
+    new FriendlyErrorsWebpackPlugin()
   ]
+};
+
+
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. Good for complex setups.
+      historyApiFallback: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT  // Defaults to 8080
+    }
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  );
+};
+
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return productionConfig();
+  }
+  return developmentConfig();
 };
