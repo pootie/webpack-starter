934fd8edd97a55d4a4a1d263f24192999c46bc91
Chris Tran <chris@grgichtran.com>
2017-09-06 05:32:59 +1000

    composing configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const merge = require('webpack-merge');
+const parts = require('./webpack.parts');
 
 const HOST = (process.env.HOST || 'localhost');
 const PORT = (process.env.PORT || 8080);
@@ -9,81 +11,44 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-const commonConfig = {
-  // Entries have to resolve to files! They rely on Node
-  // convention by default so if a directory contains *index.js*,
-  // it resolves to that.
-  entry: {
-    app: PATHS.app
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: true
-        }
-      }
+const commonConfig = merge([
+  {
+    // Entries have to resolve to files! They rely on Node
+    // convention by default so if a directory contains *index.js*,
+    // it resolves to that.
+    entry: {
+      app: PATHS.app
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack starter'
+      }),
+      new FriendlyErrorsWebpackPlugin({
+        clearConsole: false
+      })
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack starter'
-    }),
-    new FriendlyErrorsWebpackPlugin({
-      clearConsole: false
-    })
-  ]
-};
+  parts.lintJavaScript({ include: PATHS.app })
+]);
 
+const productionConfig = merge([
+]);
 
-const productionConfig = () => commonConfig;
-
-const developmentConfig = () => {
-  const config = {
-    devServer: {
-      // Enable history API fallback so HTML5 History API based
-      // routing works. Good for complex setups.
-      historyApiFallback: true,
-
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
-
-      // Parse host and port from env to allow customization.
-      //
-      // If you use Docker, Vagrant or Cloud9, set
-      // host: options.host || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices
-      // unlike default `localhost`.
-      host: HOST, // Defaults to `localhost`
-      port: PORT,  // Defaults to 8080
-      // overlay: true is equivalent
-      overlay: {
-        errors: true,
-        warnings: true
-      }
-    }
-  };
-
-  return Object.assign(
-    {},
-    commonConfig,
-    config
-  );
-};
-
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: HOST,
+    port: PORT
+  })
+]);
 
 module.exports = (env) => {
   if (env === 'production') {
-    return productionConfig();
+    return merge(commonConfig, productionConfig);
   }
-  return developmentConfig();
+  return merge(commonConfig, developmentConfig);
 };
