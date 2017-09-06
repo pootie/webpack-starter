734affdc44ccb23f7079857f0e7e4861cdc5eccd
Chris Tran <ctran@kobold.com.au>
2017-09-06 12:33:28 +1000

    loading fonts

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 export default (text = 'Hello world') => {
   const element = document.createElement('div');
 
-  element.className = 'pure-button';
+  element.className = 'fa fa-hand-spock-o fa-1g';
   element.innerHTML = text;
 
   return element;
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import component from './component';
 import 'purecss';
+import 'font-awesome/css/font-awesome.css';
 import './main.css';
 
 document.body.appendChild(component());
