ff39f892da84afb944a2fb5c10bcc28306ec1e4a
Chris Tran <ctran@kobold.com.au>
2017-09-06 13:35:10 +1000

    tree shaking

--- app/index.js
+++ app/index.js
@@ -2,5 +2,8 @@ import component from './component';
 import 'purecss';
 import 'font-awesome/css/font-awesome.css';
 import './main.css';
+import { bake } from './shake';
+
+bake();
 
 document.body.appendChild(component());
