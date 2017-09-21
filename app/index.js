4c36ad485255140eae93ea1d3f3496bdc8c253df
Chris Tran <ctran@kobold.com.au>
2017-09-21 12:03:46 +1000

    minimal webpack no frameworks with jest test

--- app/index.js
+++ app/index.js
@@ -1,9 +1,6 @@
 import component from './component';
-import 'purecss';
-import 'font-awesome/css/font-awesome.css';
 import './main.css';
 import { bake } from './shake';
-import 'vue';
 
 bake();
 
